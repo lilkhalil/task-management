cd540fccc1465264defe74f23ad8b7efdb0ebbec
Aidar Khalilov <aidarkhalilov4021@mail.ru>
2024-10-20 19:51:09 +0300

    Добавление функционала создания и просмотра задач REST API

--- manager/src/main/java/ru/mirea/manager/repository/TaskRepository.java
+++ manager/src/main/java/ru/mirea/manager/repository/TaskRepository.java
@@ -1,11 +1,14 @@
 package ru.mirea.manager.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.mirea.manager.domain.Task;
 import ru.mirea.manager.domain.enums.TaskStatus;
 
 import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
-    List<Task> findByStatus(TaskStatus taskStatus);
+    @Query("SELECT t FROM Task t WHERE (:status IS NULL OR t.status = :status)")
+    List<Task> findByStatus(@Param("status") TaskStatus taskStatus);
 }
